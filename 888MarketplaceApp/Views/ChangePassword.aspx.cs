9628377b6149bb30f276dcd8ae3bc9c0f39850d9
cyhoo <cyhoo0528@gmail.com>
2024-03-28 02:00:59 +0800

    change password page

---
+++ 888MarketplaceApp/Views/ChangePassword.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _888MarketplaceApp.Views
+{
+    public partial class ChangePassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ChangePasswordClick(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
