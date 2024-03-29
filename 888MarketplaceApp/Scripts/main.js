facdf3e4e9b02a10d61fdbc5a35ff536c63c78fc
wailoc25 <frostwl25@gmail.com>
2024-03-30 00:03:08 +0800

    stylesheet retouch

--- 888MarketplaceApp/Scripts/main.js
+++ 888MarketplaceApp/Scripts/main.js
@@ -1,23 +1,8 @@
-/*  ---------------------------------------------------
-    Template Name: Ogani
-    Description:  Ogani eCommerce  HTML Template
-    Author: Colorlib
-    Author URI: https://colorlib.com
-    Version: 1.0
-    Created: Colorlib
----------------------------------------------------------  */
-
 'use strict';
 
 (function ($) {
-
-    /*------------------
-        Preloader
-    --------------------*/
     
     $(window).on('load', function () {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
 
         //------------------
         //    Gallery filter
@@ -39,29 +24,6 @@
     $('.set-bg').each(function () {
         var bg = $(this).data('setbg');
         $(this).css('background-image', 'url(' + bg + ')');
-    });
-
-    //Humberger Menu
-    /*
-    $(".humberger__open").on('click', function () {
-        $(".humberger__menu__wrapper").addClass("show__humberger__menu__wrapper");
-        $(".humberger__menu__overlay").addClass("active");
-        $("body").addClass("over_hid");
-    });
-
-    $(".humberger__menu__overlay").on('click', function () {
-        $(".humberger__menu__wrapper").removeClass("show__humberger__menu__wrapper");
-        $(".humberger__menu__overlay").removeClass("active");
-        $("body").removeClass("over_hid");
-    });
-    */
-
-    /*------------------
-		Navigation
-	--------------------*/
-    $(".mobile-menu").slicknav({
-        prependTo: '#mobile-menu-wrap',
-        allowParentLinks: true
     });
 
     /*-----------------------
@@ -163,27 +125,6 @@
         autoplay: true
     });
 
-    /*-----------------------
-		Price Range Slider
-	------------------------ */
-    var rangeSlider = $(".price-range"),
-        minamount = $("#minamount"),
-        maxamount = $("#maxamount"),
-        minPrice = rangeSlider.data('min'),
-        maxPrice = rangeSlider.data('max');
-    rangeSlider.slider({
-        range: true,
-        min: minPrice,
-        max: maxPrice,
-        values: [minPrice, maxPrice],
-        slide: function (event, ui) {
-            minamount.val('$' + ui.values[0]);
-            maxamount.val('$' + ui.values[1]);
-        }
-    });
-    minamount.val('$' + rangeSlider.slider("values", 0));
-    maxamount.val('$' + rangeSlider.slider("values", 1));
-
     /*--------------------------
         Select
     ----------------------------*/
@@ -201,28 +142,6 @@
                 src: imgurl
             });
         }
-    });
-
-    /*-------------------
-		Quantity change
-	--------------------- */
-    var proQty = $('.pro-qty');
-    proQty.prepend('<span class="dec qtybtn">-</span>');
-    proQty.append('<span class="inc qtybtn">+</span>');
-    proQty.on('click', '.qtybtn', function () {
-        var $button = $(this);
-        var oldValue = $button.parent().find('input').val();
-        if ($button.hasClass('inc')) {
-            var newVal = parseFloat(oldValue) + 1;
-        } else {
-            // Don't allow decrementing below zero
-            if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
-            } else {
-                newVal = 0;
-            }
-        }
-        $button.parent().find('input').val(newVal);
     });
 
 })(jQuery);
--- 888MarketplaceApp/Views/Product.aspx
+++ 888MarketplaceApp/Views/Product.aspx
@@ -1,5 +1,36 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Product.aspx.cs" Inherits="_888MarketplaceApp.Views.Product" MasterPageFile="~/Views/Site.Master"%>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Product.aspx.cs" Inherits="_888MarketplaceApp.Views.Product" MasterPageFile="~/Views/Site.Master" ClientIDMode="Static" %>
 
 <asp:Content runat="server" ID="bodyCT" ContentPlaceHolderID="bodyContent">
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+
+    <div class="container">
+        <div class="row">
+            Category
+        </div>
+        <div class="card mb-4">
+            <div class="row">
+                <div class="col-md-7 mt-5 ms-3">
+                    <asp:Image ID="imgPdt" runat="server" Height="80%" ImageUrl="#" Width="80%" />
+                    <div class="mt-3">
+                        <asp:ImageButton ID="imgBtnPdt1" runat="server" CssClass="me-3" Width="50px" Height="50px" ImageUrl="~/Images/orange_shoe_1.jpg" />
+                        <asp:ImageButton ID="imgBtnPdt2" runat="server" CssClass="me-3" Width="50px" Height="50px" ImageUrl="~/Images/orange_shoe_2.jpg" />
+                    </div>
+                </div>
+                <div class="col-md-5 mt-5 me-3">
+                    <div class="form-group">
+                        <asp:Label ID="lblPdtName" runat="server" Text="Product " ></asp:Label>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label ID="lblPdtDesc" runat="server" Text="Description: " ></asp:Label>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label ID="lblPdtPrice" runat="server" Text="Price (RM): " ></asp:Label>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label ID="lblPdtQty" runat="server" Text="Stock Quantity (Unit): " ></asp:Label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </asp:Content>
