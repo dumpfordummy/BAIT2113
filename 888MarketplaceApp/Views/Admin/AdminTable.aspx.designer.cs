ab8e598e332e48251f40bf086469aba02c6d9efd
WymanHoh <85876338+WymanHoh@users.noreply.github.com>
2024-05-09 22:19:54 +0800

    Admin table with search function update

--- 888MarketplaceApp/Views/Admin/AdminTable.aspx.designer.cs
+++ 888MarketplaceApp/Views/Admin/AdminTable.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace _888MarketplaceApp
     {
 
         /// <summary>
+        /// searchBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox searchBox;
+
+        /// <summary>
         /// AdminTableRepeater control.
         /// </summary>
         /// <remarks>
